3e4fe772d0aee57c1eec938bfece26b68af1802e
asaabey <asaabey@gmail.com>
2022-09-18 15:58:32 +0930

    roxygen2 comments

--- R/compile_to_sql.R
+++ R/compile_to_sql.R
@@ -1,3 +1,16 @@
+
+#' Compile picorule text to SQL
+#'
+#' @param picorule_name Name of the picorule block (AKA block_id)
+#' @param picorule_text Body of the picorule syntax
+#' @param target_sql Target SQL framework
+#' @param compiler_api URI for API
+#'
+#' @return text with SQL code
+#' @export
+#'
+#' @examples compile_to_sql("ckd","body..","PLSQL","http://localhost:8321/v1/engine/compile")
+#'
 compile_to_sql <- function(picorule_name, picorule_text, target_sql, compiler_api){
 
   if(missing(target_sql)){
@@ -18,6 +31,7 @@ compile_to_sql <- function(picorule_name, picorule_text, target_sql, compiler_ap
 
   req_body_json <-jsonlite::toJSON(req_body, pretty = T, auto_unbox = T)
 
+  # Call API
   res <- httr::POST(
     compiler_api,
     body = req_body_json,
